6d742754eaadf1532f54234afce3577a37f8c25e
Rama-san <146070710+Rama-Alsadeq@users.noreply.github.com>
2025-09-26 15:41:10 +0300

    Update osteo_app.py

--- osteo_app.py
+++ osteo_app.py
@@ -605,4 +605,6 @@ def about():
 
 if __name__ == "__main__":
     import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
